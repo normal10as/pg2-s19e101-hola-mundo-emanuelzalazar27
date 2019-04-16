c7bc3dd1c65cae94d1563e139212c6466ccc292c
Emanuel <ema008@hotmail.com>
2019-04-15 22:13:14 -0300

    Segunda Modificacion

--- JelowMundo/Program.vb
+++ JelowMundo/Program.vb
@@ -2,6 +2,6 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World 2019!")
+        Console.WriteLine("Hello World 2019!!")
     End Sub
 End Module
